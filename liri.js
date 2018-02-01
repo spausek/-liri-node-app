4a65021733a2f9ff3e28be1bb84b37beb8075ae6
Kyle Cohen <spausek@gmail.com>
2018-02-01 15:08:49 -0800

    Added myTweets function to display twitter data

--- liri.js
+++ liri.js
@@ -6,6 +6,7 @@ var client = new twitter(keys.twitter);
 var spotify = require('node-spotify-api');
 var Spotify = new spotify(keys.spotify);
 var commands = process.argv[2];
+var inputString = process.argv
 var fs = require('fs');
 
 //Switch case to execute commands
@@ -22,3 +23,32 @@ switch(commands) {
 	default: console.log("Please enter one of the following commands; my-tweets, spotify-this-song, movie-this, or do-what-it-says");
 
 };
+
+//functions
+
+function showTweets(commands){
+	var username = inputString[3];
+
+	if(!username){
+			username ="RagnarGoldburg";
+		}
+		params = {screen_name: username};
+		client.get("statuses/user_timeline/", params, function(error, data, response){
+			if (!error) {
+				for(var i = 0; i < data.length; i++) {
+				
+					var twitterResults = 
+					"@" + data[i].user.screen_name + ": " + data[i].text;
+					console.log(twitterResults);
+					console.log(twitterResults); 
+				}
+			}  else {
+				console.log("Error :"+ error);
+				return;
+			}
+		});
+
+
+
+
+}
