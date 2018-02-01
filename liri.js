4d9a443b77dc9fcc247ddc59ed9adde81d0f3a70
Kyle Cohen <spausek@gmail.com>
2018-02-01 14:01:28 -0800

    Added variables to save twitter and spotify API keys and a switch case that will control what functions are called depending on the input recieved

--- liri.js
+++ liri.js
@@ -1,7 +1,24 @@
 //Node imports
 require("dotenv").config();
-var keys = require('./keys.js');
+var keys = require('./key.js');
 var twitter = require('twitter');
-var spotify = require('spotify');
-var command = process.argv[2];
+var client = new twitter(keys.twitter);
+var spotify = require('node-spotify-api');
+var Spotify = new spotify(keys.spotify);
+var commands = process.argv[2];
 var fs = require('fs');
+
+//Switch case to execute commands
+
+switch(commands) {
+	case 'my-tweets': showTweets();
+	break;
+	case 'spotify-this-song': spotifySong();
+	break;
+	case 'movie-this': movies();
+	break;
+	case 'do-what-it-says': doWhatItSays();
+	break;
+	default: console.log("Please enter one of the following commands; my-tweets, spotify-this-song, movie-this, or do-what-it-says");
+
+};
